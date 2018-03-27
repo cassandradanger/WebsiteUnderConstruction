3d454c69e132679340fb893fd6656256084fbe64
Casie Lynch <casie@primeacademy.io>
2018-03-26 22:11:24 -0500

    initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="hello">
+    <h1>Hi!</h1>
+    <h2>I'm busy working on an updated website.</h2>
+    <h3>Please dance like Ron Swanson for now, and feel free to email me at <a href="mailto:casie.siekman@gmail.com">casie.siekman@gmail.com</a> if you have any questions.</h3>
+    <img src="../assets/drunk_ron_swanson.gif"/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  created () {
+    /* eslint-disable no-console */
+    console.log('Oh hi there! You`re a sneaky devil. Check out my LinkedIn for my most up-to-date info: linkedin.com/in/casie-siekman')
+  }
+}
+
+</script>
+
+<style scoped>
+
+</style>
